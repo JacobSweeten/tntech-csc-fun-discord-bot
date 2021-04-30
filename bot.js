a8ddf6a6e2bef676acc12a4883a3bf7f83b210ca
JacobSweeten <sweeten.jacob@gmail.com>
2021-04-29 21:07:30 -0500

    Added command and OK Boomer functionality

--- bot.js
+++ bot.js
@@ -3,6 +3,11 @@ const Discord = require('discord.js');
 const ini = require('ini');
 
 // Initialize logging
+if(!fs.existsSync("./logs"))
+{
+	fs.mkdirSync("logs");
+}
+
 var logNum = 1
 while(fs.existsSync("./logs/log" + logNum)) {logNum++;}
 
@@ -13,7 +18,7 @@ function log(message, level = "INFO")
 	dateString = dateString.replace(/T/, " ");
 	dateString = dateString.split(".")[0];
 	entry = "[" + dateString + "]" + " " + level + "\t" + message + "\n";
-	console.log(entry)
+	console.log(entry.substring(0, entry.length - 1));
 	try
 	{
 		fs.appendFileSync("logs/log" + logNum, entry);
@@ -47,12 +52,12 @@ catch(err)
 	try
 	{
 		fs.writeFileSync("./config.ini", "[Discord]\nsecret=SECRET HERE");
-		console.log("No config file found. Created blank config file for you. Please modify the config file and restart.");
+		log("No config file found. Created blank config file for you. Please modify the config file and restart.");
 		process.exit(1);
 	}
 	catch(err)
 	{
-		console.error("Unable to read or write config file. Please ensure I have the appropriat permissions.");
+		error("Unable to read or write config file. Please ensure I have the appropriat permissions.");
 		process.exit(1);
 	}
 }
@@ -63,23 +68,41 @@ var config = ini.parse(configFile);
 
 if(config.Discord === undefined)
 {
-	console.error("No [Discord] header found in config file. Exiting.");
+	error("No [Discord] header found in config file. Exiting.");
 	process.exit(1);
 }
 
 if(config.Discord.secret === undefined)
 {
-	console.error("No API secret found in config file. Exiting.");
+	error("No API secret found in config file. Exiting.");
 	process.exit(1);
+}
+
+async function okBoomer(msgID, msg)
+{
+	var targetMsg = await msg.channel.messages.fetch(msgID);
+	if(targetMsg === undefined)
+	{
+		msg.reply("That is an invalid message ID or it is not in this channel.");
+	}
+
+	await targetMsg.react("🆗");
+	await targetMsg.react("🅱️");
+	await targetMsg.react("🇴");
+	await targetMsg.react("🅾️");
+	await targetMsg.react("🇲");
+	await targetMsg.react("🇪");
+	await targetMsg.react("🇷");
+
 }
 
 const client = new Discord.Client();
 
 client.on("ready", () => {
-	console.log("Connected.");
-	
-	client.user.setActivity("?help");
-	setInterval(()=>{client.user.setActivity("?help");}, 3600000);
+	log("Discord connected.");
+
+	client.user.setActivity("+help");
+	setInterval(()=>{client.user.setActivity("+help");}, 3600000);
 });
 
 client.on("message", (msg) => {
@@ -87,7 +110,24 @@ client.on("message", (msg) => {
 	if(msg.author.id === "771905394142216202")
 		return;
 
-	
+	if(msg.content.startsWith("+"))
+	{
+		var msgArr = msg.content.split(" ");
+		var command = msgArr[0].substring(1);
+		var arg = msgArr.splice(1).join(" ");
+		
+		log("Command from user \"" + msg.author.username + "\": \"" + command + "\" Argument: \"" + arg + "\"");
+
+		switch(command)
+		{
+			case "help":
+				msg.reply("```?okb to OK Boomer a message!\nMore commands to come!```");
+				break;
+			case "okb":
+				okBoomer(arg, msg)
+				break;
+		}
+	}
 });
 
 client.login(config.Discord.secret)
